d0e9d4300a31b903f23ab5a77695593ed277964f
Zbigniew Koza <zbigniew.koza@uwr.edu.pl>
2024-05-09 00:53:22 +0200

    zadania 1 i 2 nieco lepiej napisane. Proszę porównać ze swoją wersją i wyciągać wnioski.

--- lista4_cpp/zadanie1.cpp
+++ lista4_cpp/zadanie1.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 
-double operator*(const std::vector<double>& v, const std::vector<double>& w) {
-    if (v.size() != w.size()) {
-        std::cerr << "Błąd: Wektory muszą mieć tę samą długość!" << std::endl;
-        return 0; 
+double operator*(const std::vector<double>& v, const std::vector<double>& w)
+{
+    if (v.size() != w.size())
+    {
+        throw std::logic_error("Błąd: Wektory muszą mieć tę samą długość!");
     }
-    
-    double result = 0;
-    for (size_t i = 0; i < v.size(); ++i) {
+
+    double result = 0.0;
+    for (size_t i = 0; i < v.size(); ++i)
+    {
         result += v[i] * w[i];
     }
     return result;
 }
 
-int main() {
-    std::vector<double> v1;
-    v1.push_back(1);
-    v1.push_back(2);
-    v1.push_back(3);
-
-    std::vector<double> v2;
-    v2.push_back(5);
-    v2.push_back(6);
-    v2.push_back(7);
+int main()
+{
+    std::vector<double> v1 {1, 2, 3};
+    std::vector<double> v2 {5, 6, 7};
 
     double dot_product = v1 * v2;
-    std::cout << "Iloczyn skalarny v1 i v2: " << dot_product << std::endl;
+    std::cout << "Iloczyn skalarny v1 i v2: " << dot_product << "\n";
 
-    return 0;
+    return EXIT_SUCCESS;
 }
